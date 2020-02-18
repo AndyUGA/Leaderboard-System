0828cb019b241850563eba2ece3e2f567ef35e09
Andy <att35663@uga.edu>
2020-02-17 20:08:31 -0500

    Implemented scanner

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
 const app = express();
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 
 app.get('/', (req, res) => {
+
     res.send("Hello World!");
 })
 
 
+app.get('/scanner', (req, res) => {
+    res.render("scanner");
+})
 
+
+
+/*
 const uri = "mongodb+srv://andy:test123@cluster0-iuoam.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("element3");
-  collection.insertOne( { item: "card", qty: 15 } );
 });
+*/
 
 
 
